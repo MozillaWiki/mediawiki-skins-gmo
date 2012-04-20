8d50cc3a07f62fad054a07f1a3dd4079f68010f7
Mark A. Hershberger <mah@everybody.org>
2012-04-20 17:18:51 -0400

    * Add an extension file so the source for the skin and credits gets shown on [Special:Version].  Extension file also includes some basic config info. * Add i18n file for messages on [Special:Version].  This also makes them translatable for whoever wants that.

---
+++ skins/gmo.i18n.php
@@ -0,0 +1,8 @@
+<?php
+
+$messages['en'] = array(
+	'gmo-skin' => 'Default skin for wiki.mozilla.org',
+	'skinname-gmo' => 'GMO',
+	'cavendish-skin' => 'Cavendish skin',
+	'skinname-cavendish' => 'Cavendish'
+);
