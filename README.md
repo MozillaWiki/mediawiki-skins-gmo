94322554435854e98cccd623ebbdc79a8c0cefad
MarkAHershberger <mah@everybody.org>
2013-02-26 21:09:44 -0500

    updated templates

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ GMO skin for MediaWiki
 
 This is meant to be a checked out using git submodules, so:
 
+  $ git clone https://gerrit.wikimedia.org/r/p/mediawiki/core wiki
   $ cd wiki
+  $ git checkout REL1_19
+  $ cp ~/LocalSettingsSTORAGE/LocalSettings.php .
   $ git submodule add https://github.com/mozilla/mediawiki-skins-gmo.git extensions/gmo
-  $ ln -s ../extensions/gmo/skins/gmo.php skins/
-  $ ln -s ../extensions/gmo/skins/Cavendish.php skins/
+  $ for i in extensions/gmo/skins/*; do ln -s ../$i skins ; done
+  $ awk '/(path|url) =/ {print $3}' extensions/gmo/gitmodules | xargs -n 2 git submodule add -f
--- skins/Cavendish.php
+++ skins/Cavendish.php
@@ -1,1 +1,265 @@
-cavendish/Cavendish.php
+<?php
+/**
+ * Mozilla Cavendish theme
+ *
+ * Loosely based on the monobook style by Gabriel Wicke
+ *
+ * @todo document
+ * @package MediaWiki
+ * @subpackage Skins
+ */
+
+
+if( !defined( 'MEDIAWIKI' ) )
+	die();
+
+/** */
+require_once('includes/SkinTemplate.php');
+
+/**
+ * Inherit main code from SkinTemplate, set the CSS and template filter.
+ * @todo document
+ * @package MediaWiki
+ * @subpackage Skins
+ */
+class SkinCavendish extends SkinTemplate {
+	/** Using cavendish. */
+	function initPage( &$out ) {
+		parent::initPage( $out );
+		$this->skinname  = 'cavendish';
+		$this->stylename = 'cavendish';
+		$this->template  = 'CavendishTemplate';
+	}
+
+}
+
+class CavendishTemplate extends QuickTemplate {
+	/**
+	 * Template filter callback for MonoBook skin.
+	 * Takes an associative array of data set from a SkinTemplate-based
+	 * class, and a wrapper for MediaWiki's localization database, and
+	 * outputs a formatted page.
+	 *
+	 * @access private
+	 */
+	function execute() {
+		global $wgRequest;
+		$this->skin = $this->data['skin'];
+		$this->action = $wgRequest->getText( 'action' );
+		$this->stylepath = $this->data['stylepath'].'/'.$this->data['stylename'];
+
+		// Suppress warnings to prevent notices about missing indexes in $this->data
+		wfSuppressWarnings();
+		$this->drawpage();
+		wfRestoreWarnings();
+	}
+
+	function drawpage() {
+		$this->doctype();
+		$this->head();
+		$this->header();
+		$this->body();
+		$this->footer();
+	}
+	/*************************************************************************************************/
+	function doctype($html5=false) {
+		if ($html5 == true) {
+			print('<!DOCTYPE HTML><html xml:lang="'
+				.$this->data['lang'].'" lang="'
+				.$this->data['lang'].'" dir="'
+				.$this->data['dir'].'">');
+		} else {
+			$doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"';
+			$doctype .= ' "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+			$doctype .= '<html xmlns="'.$this->data['xhtmldefaultnamespace'].'"';
+			foreach($this->data['xhtmlnamespaces'] as $tag => $ns) {
+				$doctype .= 'xmlns:'.$tag.'="'.$ns.'" ';
+			}
+			$doctype .= ' xml:lang="'.$this->data['lang'].'" lang="'.$this->data['lang'].'" dir="'.$this->data['dir'].'">';
+			print($doctype);
+		}
+	}
+
+	function head() {
+?>
+<head>
+	<meta http-equiv="Content-Type" content="<?php $this->text('mimetype') ?>; charset=<?php $this->text('charset') ?>" />
+	<?php $this->html('headlinks') ?>
+	<title><?php $this->text('pagetitle') ?></title>
+	<?php $this->html('csslinks') ?>
+	<style type="text/css" media="screen,projection">/*<![CDATA[*/ @import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/main.css"; /*]]>*/</style>
+	<link rel="stylesheet" type="text/css" media="print" href="<?php $this->text('stylepath') ?>/common/commonPrint.css" />
+	<?php print Skin::makeGlobalVariablesScript( $this->data ); ?>
+	<script type="text/javascript" src="<?php $this->text('stylepath' ) ?>/common/wikibits.js"></script>
+	<?php
+	$jsBit = array( 'jsvarurl', 'userjs', 'userjsprev' );
+	foreach( $jsBit as $bit ) {
+		if( isset( $this->data[ $bit ] ) ) {
+			echo "<script type='text/javascript' src='" . $this->text($bit) ."'></script>\n";
+		}
+	}
+	$cssBit = array( 'pagecss', 'usercss' );
+	foreach( $cssBit as $bit ) {
+		if( isset( $this->data[ $bit ] ) ) {
+			echo "<style type='text/css'>". $this->html($bit) ."</style>\n";
+		}
+	}
+	$this->html('headscripts');
+	?>
+	<script>if ( window.mediaWiki ) {
+
+		mediaWiki.loader.load(["mediawiki.util", "mediawiki.legacy.wikibits", "mediawiki.legacy.ajax", "ext.smw.sorttable"]);
+		mediaWiki.loader.go();
+	}
+	</script>
+	<script type="text/javascript"> if ( window.isMSIE55 ) fixalpha(); </script>
+</head>
+<?php
+	}
+
+	function header() {
+		global $wgStylePath;
+?>
+	<body <?php if(isset($this->data['body_ondblclick'])) { ?>ondblclick="<?php $this->text('body_ondblclick') ?>"<?php } ?>
+	<?php if(isset($this->data['nsclass'        ])) { ?>class="<?php      $this->text('nsclass')         ?>"<?php } ?>>
+
+<div id="internal"></div>
+<div id="container">
+
+	<div id="mozilla-org"><a href="http://www.mozilla.org/">Visit Mozilla.org</a></div>
+
+	<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
+	<div id="header">
+		<a name="top" id="contentTop"></a>
+		<h1><a
+		href="<?php echo htmlspecialchars($this->data['nav_urls']['mainpage']['href'])?>"
+		title="<?php $this->msg('mainpage') ?>"><?php $this->text('title') ?></a></h1>
+		<ul>
+			<?php foreach($this->data['content_actions'] as $key => $action) {
+			   ?><li
+			   <?php if($action['class']) { ?>class="<?php echo htmlspecialchars($action['class']) ?>"<?php } ?>
+			   ><a href="<?php echo htmlspecialchars($action['href']) ?>"><?php
+			   echo htmlspecialchars($action['text']) ?></a></li><?php
+			 } ?>
+		</ul>
+		<form name="searchform" action="<?php $this->text('searchaction') ?>" id="search">
+			<div>
+			<label for="q"><?php $this->msg('search') ?></label>
+			<input id="q" name="search" type="text"
+			<?php if($this->haveMsg('accesskey-search')) {
+			  ?>accesskey="<?php $this->msg('accesskey-search') ?>"<?php }
+			if( isset( $this->data['search'] ) ) {
+			  ?> value="<?php $this->text('search') ?>"<?php } ?> />
+			<input type="submit" name="go" class="searchButton" id="searchGoButton"
+			value="<?php $this->msg('go') ?>"
+			/>&nbsp;<input type="submit" name="fulltext"
+			class="searchButton"
+			value="<?php $this->msg('search') ?>" />
+		   </div>
+		</form>
+	</div>
+<?php
+	}
+
+	function body() {
+?>
+	<div id="mBody">
+
+		<div id="side">
+			<ul id="nav">
+				<li><span><?php $this->msg('personaltools') ?></span>
+					<ul>
+					<?php foreach($this->data['personal_urls'] as $key => $item) {
+					   ?><li id="pt-<?php echo htmlspecialchars($key) ?>"><a href="<?php
+					   echo htmlspecialchars($item['href']) ?>"<?php
+					  if(!empty($item['class'])) { ?> class="<?php
+					   echo htmlspecialchars($item['class']) ?>"<?php } ?>><?php
+					   echo htmlspecialchars($item['text']) ?></a></li><?php
+					} ?>
+					</ul>
+				</li>
+				<li><span><?php $this->msg('navigation') ?></span>
+					<ul>
+					   <?php foreach ($this->data['sidebar'] as $bar => $cont) { ?>
+					   <?php foreach($cont as $key => $val) { ?>
+						 <li id="<?php echo htmlspecialchars($val['id']) ?>"><a href="<?php echo htmlspecialchars($val['href']) ?>"><?php echo htmlspecialchars($val['text'])?></a></li>
+						<?php } ?>
+				<?php } ?>
+
+					</ul>
+				</li>
+				<li><span><?php $this->msg('toolbox') ?></span>
+					<ul>
+					  <?php if($this->data['notspecialpage']) { foreach( array( 'whatlinkshere', 'recentchangeslinked' ) as $special ) { ?>
+					  <li id="t-<?php echo $special?>"><a href="<?php
+						echo htmlspecialchars($this->data['nav_urls'][$special]['href'])
+						?>"><?php echo $this->msg($special) ?></a></li>
+					  <?php } } ?>
+					  <?php if($this->data['feeds']) { ?><li id="feedlinks"><?php foreach($this->data['feeds'] as $key => $feed) {
+						?><span id="feed-<?php echo htmlspecialchars($key) ?>"><a href="<?php
+						echo htmlspecialchars($feed['href']) ?>"><?php echo htmlspecialchars($feed['text'])?></a>&nbsp;</span>
+						<?php } ?></li><?php } ?>
+					  <?php foreach( array('contributions', 'emailuser', 'upload', 'specialpages') as $special ) { ?>
+					  <?php if($this->data['nav_urls'][$special]) {?><li id="t-<?php echo $special ?>"><a href="<?php
+						echo htmlspecialchars($this->data['nav_urls'][$special]['href'])
+						?>"><?php $this->msg($special) ?></a></li><?php } ?>
+					  <?php } ?>
+					</ul>
+				</li>
+				<?php if( $this->data['language_urls'] ) { ?>
+					<li><span><?php $this->msg('otherlanguages') ?></span>
+						<ul>
+						<?php foreach($this->data['language_urls'] as $langlink) { ?>
+							<li>
+							<a href="<?php echo htmlspecialchars($langlink['href'])
+							?>"><?php echo $langlink['text'] ?></a>
+							</li>
+						<?php } ?>
+						</ul>
+					</li>
+				<?php } ?>
+			</ul>
+		</div><!-- end of SIDE div -->
+
+		<div id="mainContent">
+
+			<h1><?php $this->text('title') ?></h1>
+			<h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
+			<div id="contentSub"><?php $this->html('subtitle') ?></div>
+			<?php if($this->data['undelete']) { ?><div id="contentSub"><?php     $this->html('undelete') ?></div><?php } ?>
+			<?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
+			<!-- start content -->
+			<?php $this->html('bodytext') ?>
+			<?php if($this->data['catlinks']) { ?><div id="catlinks"><?php       $this->html('catlinks') ?></div><?php } ?>
+			<!-- end content -->
+
+		</div><!-- end of MAINCONTENT div -->
+
+	</div><!-- end of MBODY div -->
+<?php
+    }
+
+	function footer() {
+?>
+	<div id="footer">
+		<?php if($this->data['copyrightico']) { ?><div id="f-copyrightico"><?php $this->html('copyrightico') ?></div><?php } ?>
+		<?php if($this->data['lastmod'   ]) { ?><span id="f-lastmod"><?php    $this->html('lastmod')    ?></span><?php } ?>
+		<?php if($this->data['viewcount' ]) { ?><span id="f-viewcount"><?php  $this->html('viewcount')  ?> </span><?php } ?>
+		<ul id="f-list">
+			<?php if($this->data['credits'   ]) { ?><li id="f-credits"><?php    $this->html('credits')    ?></li><?php } ?>
+			<?php if($this->data['copyright' ]) { ?><li id="f-copyright"><?php  $this->html('copyright')  ?></li><?php } ?>
+			<?php if($this->data['about'     ]) { ?><li id="f-about"><?php      $this->html('about')      ?></li><?php } ?>
+			<?php if($this->data['disclaimer']) { ?><li id="f-disclaimer"><?php $this->html('disclaimer') ?></li><?php } ?>
+			<li><a href="http://www.mozilla.org/about/policies/privacy-policy.html">Privacy Policy</a></li>
+		</ul>
+		<?php if($this->data['poweredbyico']) { ?><div id="f-poweredbyico"><?php $this->html('poweredbyico') ?></div><?php } ?>
+	</div><!-- end of the FOOTER div -->
+</div><!-- end of the CONTAINER div -->
+
+<?php $this->html('reporttime') ?>
+
+</body>
+</html>
+<?php
+	}
+}
